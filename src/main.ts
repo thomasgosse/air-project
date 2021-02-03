d3019655e245408a9d65830e1bd0167cea23b019
Thomas Gosse <thms.gosse@gmail.com>
2021-02-03 20:30:40 +0100

    fix: remove vue-select dependency

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { store } from "./store";
-import vSelect from "vue-select";
 
 import init from "./init";
 
@@ -11,5 +10,4 @@ init();
 createApp(App)
   .use(store)
   .use(router)
-  .component("v-select", vSelect)
   .mount("#app");
